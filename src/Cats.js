64d525ed0e526b890e12510b37f57da81f93ae84
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-10 12:00:03 -0500

    dynamic cat names from this.state.catToys

--- src/Cats.js
+++ src/Cats.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Cats = props => {
+  const catListItems = props.catList.map(catName => {
+    return (
+      <li>
+        <Link to={`/cats/${catName}`}>{catName}</Link>
+      </li>
+    );
+  });
   return (
     <div>
       <h1>Cats</h1>
-      <ul>
-        <li>
-          <Link to="/cats/oakley">Oakley</Link>
-        </li>
-        <li>
-          <Link to="/cats/milla">Milla</Link>
-        </li>
-      </ul>
+      <ul>{catListItems}</ul>
     </div>
   );
 };
