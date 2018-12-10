0eca9a010a4c08f6c4b27395077f108ee41e4c8a
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-10 10:59:08 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import Cats from "./Cats";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cats" component={Cats} />
+        </div>
+      </Router>
     );
   }
 }
